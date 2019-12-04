62e4d363b70a0f6447e72debd91518a1c172f241
pragnaS <36314074+pragnaS@users.noreply.github.com>
2019-12-03 22:41:19 -0600

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -1,1 +1,21 @@
-//hashtable.h
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class hashTable{
+
+    private:
+        struct HashNode{
+            int value;
+            HashNode* next;
+        };
+
+    public:
+        hashTable();
+        void hashFunction(string &word, int key);
+        int* detectCollision(int numFiles, vector<string> &files);
+        ~hashTable();
+
+};
