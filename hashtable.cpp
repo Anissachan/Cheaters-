7019d182ec5260ecf7fcacd2c559745f09385341
pragnaS <36314074+pragnaS@users.noreply.github.com>
2019-12-04 13:13:50 -0600

    Update hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,1 +1,29 @@
-//hashtable.cpp
+#include "hashtable.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+void hashTable::hashFunction(string &word, int key) {
+    int index = 0;
+    int multiplier = 13;
+    int num = 1;
+
+    for(int i = 0 ; i< word.length(); i = i+2){
+        multiplier = num * multiplier;
+        index = index + (word[i]*multiplier);
+    }
+
+    index = index%50003;
+
+    HashNode* temp = new HashNode;
+    temp ->value = key;
+
+    if(table[index] == NULL){
+        temp->next = NULL;
+        table[index] = temp;
+    }
+    else{
+        temp->next = table[index];
+        table[index] = temp;
+    }
+}
