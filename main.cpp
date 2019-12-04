9338966b137a597c8c6fa4a232990c649bddef02
Anissachan <45741414+Anissachan@users.noreply.github.com>
2019-12-04 14:31:06 -0600

    Update main.cpp
    fixing the hashtable pointer

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main() {
     string dir = string("sm_doc_set");
     int n = 4;
     int limit = 200;
-    hashTable *HashTable = new hashTable;
+    hashTable *HashTable;
 
     vector<string> files = vector<string>();
     getdir(dir, files);
